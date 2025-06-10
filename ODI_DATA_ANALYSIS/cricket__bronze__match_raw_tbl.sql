6b219c941dbc97559a527984781de20ec588611e
Prakhar-ui <prakha8380@gmail.com>
2025-06-10 13:20:22 +0530

    cricket__bronze__match_raw_tbl

---
+++ ODI_DATA_ANALYSIS/cricket__bronze__match_raw_tbl.sql
@@ -0,0 +1,33 @@
+use role sysadmin;
+use warehouse sysadmin_wh;
+use schema cricket.bronze;
+
+create or replace transient table cricket.bronze.match_raw_tbl (
+    meta object not null,
+    info variant not null,
+    innings array not null,
+    stg_file_name text not null,
+    stg_file_row_number int not null,
+    stg_file_hashkey text not null,
+    stg_modified_ts timestamp not null
+)
+comment = 'This is raw table to store all the json data file with root elements extracted'
+;
+
+copy into cricket.bronze.match_raw_tbl from (
+    select 
+    t.$1:meta::variant as meta,
+    t.$1:info::variant as info,
+    t.$1:innings::array as innings,
+    --
+    metadata$filename,
+    metadata$file_row_number,
+    metadata$file_content_key,
+    metadata$file_last_modified
+from @cricket.lz.my_stg/cricket/json (file_format => 'cricket.lz.my_json_format') t 
+)
+on_error = continue;
+
+select count(*) from cricket.bronze.match_raw_tbl;
+
+select * from cricket.bronze.match_raw_tbl limit 10;
