bcde0bef4e35e576a5bb5c80f3552fb739faed70
Prakhar-ui <prakha8380@gmail.com>
2025-06-10 13:20:35 +0530

    cricket__silver__match_details_clean

---
+++ ODI_DATA_ANALYSIS/cricket__silver__match_details_clean.sql
@@ -0,0 +1,83 @@
+use role sysadmin;
+use warehouse sysadmin_wh;
+use schema cricket.silver;
+
+-- select 
+--     meta['data_version']::text as data_version,
+--     meta['created']::date as created,
+--     meta['revision']::number as revision
+-- from 
+--     cricket.bronze.match_raw_tbl;
+
+-- select 
+--     info:match_type_number::int as match_type_number,
+--     info:event.name::text as event_name,
+--     coalesce(info:event.match_number::text, 
+--             info:event.stage::text,
+--             'NA') as match_stage,
+--     info:dates[0]::date as event_date,
+--     date_part('year', info:dates[0]::date) as event_year,
+--     date_part('month', info:dates[0]::date) as event_month,
+--     date_part('day', info:dates[0]::date) as event_day,
+--     info:match_type::text as match_type,
+--     info:season::text as season,
+--     info:team_type::text as team_type,
+--     info:overs::text as overs,
+--     coalesce(info:city::text, 'NA') as city,
+--     info:venue::text as venue,
+--     info:gender::text as gender,
+--     info:teams[0]::text as first_team,
+--     info:teams[1]::text as second_team,
+--     case
+--         when info:outcome.winner is not null then 'Result Declared'
+--         when info:outcome.result = 'tie' then 'Tie'
+--         when info:outcome.result = 'no result' then 'No Result'
+--         else info:outcome.result
+--     end as match_result,
+--     coalesce(info:outcome.winner, 'NA') as winner,
+--     info:toss.winner::text as toss_winner,
+--     initcap(info:toss.decision::text) as toss_decision
+-- from 
+--     cricket.bronze.match_raw_tbl;
+
+    
+create or replace transient table cricket.silver.match_details_clean as 
+select 
+    info:match_type_number::int as match_type_number,
+    info:event.name::text as event_name,
+    coalesce(info:event.match_number::text, 
+            info:event.stage::text,
+            'NA') as match_stage,
+    info:dates[0]::date as event_date,
+    date_part('year', info:dates[0]::date) as event_year,
+    date_part('month', info:dates[0]::date) as event_month,
+    date_part('day', info:dates[0]::date) as event_day,
+    info:match_type::text as match_type,
+    info:season::text as season,
+    info:team_type::text as team_type,
+    info:overs::text as overs,
+    coalesce(info:city::text, 'NA') as city,
+    info:venue::text as venue,
+    info:gender::text as gender,
+    info:teams[0]::text as first_team,
+    info:teams[1]::text as second_team,
+    case
+        when info:outcome.winner is not null then 'Result Declared'
+        when info:outcome.result = 'tie' then 'Tie'
+        when info:outcome.result = 'no result' then 'No Result'
+        else info:outcome.result
+    end as match_result,
+    coalesce(info:outcome.winner, 'NA') as winner,
+    info:toss.winner::text as toss_winner,
+    initcap(info:toss.decision::text) as toss_decision,
+    --
+    stg_file_name,
+    stg_file_row_number,
+    stg_file_hashkey,
+    stg_modified_ts
+from 
+    cricket.bronze.match_raw_tbl;
+
+
+    
+    
