3b6450e6bc385f79385dd498f1ecf1ee65036fdd
Prakhar-ui <prakha8380@gmail.com>
2025-06-18 12:39:56 +0530

    fixed structure

--- odi_cricket_data_analysis/Tasks/dim/cricket__bronze__load_to_player_dim.sql
+++ odi_cricket_data_analysis/Tasks/dim/cricket__bronze__load_to_player_dim.sql
@@ -4,8 +4,8 @@ create or replace task cricket.bronze.load_to_player_dim
         as 
     insert into cricket.gold.player_dim (team_id, player_name)  
     select team.team_id, pl.player_name
-    from cricket.silver.player_clean_tbl pl join
-    cricket.gold.team_dim team
+    from cricket.silver.player_clean_tbl pl 
+    join cricket.gold.team_dim team
     on pl.country = team.team_name
     group by team.team_id, pl.player_name
     minus 
